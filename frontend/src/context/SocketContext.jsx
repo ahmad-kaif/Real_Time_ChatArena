e8ad7f15bb0b1408d825dd9c961509c037599f7d
Ahmad Kaif <heyahmad611@gmail.com>
2024-07-19 18:04:16 +0530

    .

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("https://chatarena-frpx.onrender.com/login",{
+            const socket = io("https://chatarena-frpx.onrender.com",{
                 query:{
                     userId:authUser._id,
                 },
