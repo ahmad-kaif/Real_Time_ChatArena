51d5a083808907d93eaa6365644ca8f0e60d22f2
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-20 11:47:34 +0530

    .

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,42 +1,46 @@
-import {  createContext, useContext, useEffect, useState } from "react";
-import {  useAuthContext } from "./AuthContext";
-import io from "socket.io-client";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
+import { io } from "socket.io-client";
 
 const SocketContext = createContext();
 
 export const useSocketContext = () => {
-    return useContext(SocketContext);
-}
+  return useContext(SocketContext);
+};
 
-export const SocketContextProvider = ({children}) => {
-    const [socket, setSocket] = useState(null);
-    const [onlineUsers, setOnlineUsers] = useState([]);
-    const {authUser} = useAuthContext();
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
 
-    useEffect(() => {
-        if(authUser){
-            const socket = io("https://chatarena-frpx.onrender.com",{
-                query:{
-                    userId:authUser._id,
-                },
-            });
+  const socketUrl =
+    process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://chatarena-frpx.onrender.com";
 
-            setSocket(socket);
+  useEffect(() => {
+    if (authUser) {
+        const socket = io(socketUrl, {
+            query: { userId: authUser._id },
+          });
 
-            socket.on("getOnlineUsers", (users) => {
-                setOnlineUsers(users);
-            })
+      setSocket(socket);
 
-            return () => socket.close();
-        }else{
-            if(socket){
-                socket.close();
-                setSocket(null);
-            }
-        }
-    },[authUser])
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
 
-    return(
-        <SocketContext.Provider value={{socket,onlineUsers}}> {children} </SocketContext.Provider>
-    )
-}
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {" "}
+      {children}{" "}
+    </SocketContext.Provider>
+  );
+};
